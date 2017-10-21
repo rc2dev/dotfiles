2800d0788838d22ffe92ec3665b73605398baf4a
Rafael Cavalcanti <rccavalcanti@gmail.com>
2017-10-21 18:42:18 -0200

    Add Firefox configuration

---
+++ arch/firefox/.mozilla/firefox/config/user.js
@@ -0,0 +1,47 @@
+// Minhas alterações ao about:config do Firefox
+//
+// Rafael Cavalcanti
+
+
+// Arch Wiki
+user_pref("security.ssl3.dhe_rsa_aes_256_sha", false);
+user_pref("security.ssl3.dhe_rsa_aes_128_sha", false);
+user_pref("geo.enabled", false);
+user_pref("webgl.disabled", true);
+
+// Ativado pela GUI no Nightly: Tracking protection - always
+user_pref("privacy.trackingprotection.enabled", true);
+
+// Desativar Pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Pra aparecer OpenGL no about:support - Graphics (OpenGL Off-Main-Thread
+// Compositing - OMTC - Arch Wiki). Acho que ajudou com tearing.
+user_pref("layers.acceleration.force-enabled", true);
+
+// Não mostrar aviso de tela cheia
+user_pref("full-screen-api.warning.timeout", 0);
+
+// Desativar aviso do about:config
+user_pref("general.warnOnAboutConfig", false);
+
+// Não fechar navegador ao fechar última aba
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// Abrir "Buscar no Google" do menu de contexto em segundo plano
+user_pref("browser.search.context.loadInBackground", true);
+
+// Daqui pra baixo, o que mexi pela GUI e achei no pref.js
+////Restaurar sessão ao iniciar (fiz pela GUI)
+//user_pref("browser.startup.page", 3);
+
+////Trocar fontes em para as usadas no Chrome (fiz pela GUI em "Advanced")
+//user_pref("font.name.monospace.x-western", "monospace");
+//user_pref("font.name.sans-serif.x-western", "Arial");
+//user_pref("font.name.serif.x-western", "Times New Roman");
+
+//// Adicionar Blogtrotrr como feed reader (aparente resultado de ter clicado no link
+//// para tal no site deles)
+//user_pref("browser.contentHandlers.types.6.title", "Blogtrottr");
+//user_pref("browser.contentHandlers.types.6.type", "application/vnd.mozilla.maybe.feed");
+//user_pref("browser.contentHandlers.types.6.uri", "https://blogtrottr.com/?subscribe=%s");
--- arch/firejail/.config/firejail/firefox.profile
+++ arch/firejail/.config/firejail/firefox.profile
@@ -6,6 +6,7 @@ include /etc/firejail/firefox.profile
 # Diretórios que uso
 whitelist ~/Imagens
 blacklist ~/Imagens/Fotos
+whitelist ~/.dotfiles/arch/firefox
 
 # Deixar só ver ~/Dropbox/tmp
 whitelist ~/Dropbox
