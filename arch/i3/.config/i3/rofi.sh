9713d31880111db1c4a769f8351553aef5b02f9a
Rafael Cavalcanti <rccavalcanti@gmail.com>
2018-03-19 06:43:50 -0300

    [Arch] i3 and Rofi changes
    - Remove Rofi wrapper script (move options to configuration file)
    - Change some windows rules on i3

--- arch/i3/.config/i3/rofi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Rafael Cavalcanti
-
-vbox="~/.config/rofi/vbox.sh"
-vnc="~/.config/rofi/vnc.sh"
-
-rofi -combi-modi window,drun,ssh,vnc:$vnc,vbox:$vbox,run -show combi \
-	-modi combi,ssh,vnc:$vnc,vbox:$vbox -display-combi ""
