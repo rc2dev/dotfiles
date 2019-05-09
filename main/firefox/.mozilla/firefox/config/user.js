e2d6c5151e554d9a751c73ea084f0b47df11754e
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-05-09 03:57:31 -0300

    Firefox: Clean-up user.js

--- main/firefox/.mozilla/firefox/config/user.js
+++ main/firefox/.mozilla/firefox/config/user.js
@@ -3,13 +3,6 @@
 // Rafael Cavalcanti
 
 
-// Arch Wiki
-user_pref("security.ssl3.dhe_rsa_aes_256_sha", false);
-user_pref("security.ssl3.dhe_rsa_aes_128_sha", false);
-user_pref("geo.enabled", false);
-// This option breaks 360 videos
-//user_pref("webgl.disabled", true);
-
 
 // ==================================================================
 // Annoyances, workflow
@@ -45,9 +38,6 @@ user_pref("browser.search.context.loadInBackground", true);
 // Restaurar sessão ao iniciar
 user_pref("browser.startup.page", 3);
 
-// Não aceitar third-party cookies
-user_pref("network.cookie.cookieBehavior", 1);
-
 // Trocar fontes para as usadas no Chrome (na GUI em "Advanced")
 user_pref("font.name.monospace.x-western", "monospace");
 user_pref("font.name.sans-serif.x-western", "Arial");
@@ -56,8 +46,3 @@ user_pref("font.name.serif.x-western", "Times New Roman");
 // Habilitar autorolagem
 user_pref("general.autoScroll", true);
 
-//// Adicionar Blogtrotrr como feed reader (aparente resultado de ter clicado no link
-//// para tal no site deles)
-//user_pref("browser.contentHandlers.types.6.title", "Blogtrottr");
-//user_pref("browser.contentHandlers.types.6.type", "application/vnd.mozilla.maybe.feed");
-//user_pref("browser.contentHandlers.types.6.uri", "https://blogtrottr.com/?subscribe=%s");
