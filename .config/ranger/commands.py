98b8fc02520d88e397dc98d184cc2bc47947d3c6
Rafael Cavalcanti <rccavalcanti@gmail.com>
2020-10-08 21:21:16 -0300

    ranger: Add fzf_locate command

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -101,3 +101,34 @@ class fzf_select(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+# Adapted from https://github.com/ranger/ranger/wiki/Custom-Commands
+# Like fzf_select, but searches globally, not only child directories.
+class fzf_locate(Command):
+    """
+    :fzf_locate
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="locate $HOME | fzf +m"
+        else:
+            # match files and directories
+            command="locate $HOME | fzf +m"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
