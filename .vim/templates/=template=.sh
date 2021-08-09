8bcaab547f33ea8e96570fa4d6048a0600ff3541
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-08-09 15:05:08 -0300

    vim: Update sh template

--- .vim/templates/=template=.sh
+++ .vim/templates/=template=.sh
@@ -8,11 +8,4 @@ set -euo pipefail
 readonly script_name="$(basename "$0")"
 readonly script_dir="$(dirname "$(readlink -f "$0")")"
 
-main() {
-  # check_args "${@}"
-  :%HERE%
-}
-
-# Helper functions
-
-main "$@"
+%HERE%
