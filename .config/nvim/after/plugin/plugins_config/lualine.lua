18c2a08d94cc04f2f6124e142a7525bb8dfbb440
Rafael Cavalcanti <dev@rafaelc.org>
2022-11-10 20:04:17 -0300

    vim: Change separator for lualine

--- .config/nvim/after/plugin/plugins_config/lualine.lua
+++ .config/nvim/after/plugin/plugins_config/lualine.lua
@@ -2,5 +2,5 @@
 -- Author: Rafael Cavalcanti <https://rafaelc.org/dev>
 
 require('lualine').setup {
-  options = { section_separators = '', component_separators = '/' },
+  options = { section_separators = '', component_separators = '·' },
 }
