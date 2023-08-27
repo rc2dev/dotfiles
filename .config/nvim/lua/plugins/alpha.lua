a82583927ace0c7aecb02622e5fc8f77f7273bbf
Rafael Cavalcanti <dev@rafaelc.org>
2023-08-27 20:54:18 -0300

    vim: Changes to alpha theme

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -4,7 +4,25 @@
 return  {
   "goolord/alpha-nvim",
   config = function()
-    require("alpha").setup(require("alpha.themes.startify").config)
-  end,
+    --require("alpha").setup(require("alpha.themes.startify").config)
+    local alpha = require("alpha")
+    local startify = require("alpha.themes.startify")
+
+    -- add l for enter
+    startify.config.opts.keymap = {
+      press = { '<CR>', 'l' },
+    }
+
+    -- Add quick note button
+    startify.section.top_buttons.val = {
+      startify.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
+      startify.button( "n", "  Quick Note" , ":e $NOTES/notes/Quick Note.md<CR>"),
+    }
+
+    -- disable MRU
+    startify.section.mru.val = { { type = "padding", val = 0 } }
+
+    alpha.setup(startify.config)
+  end
 }
 
