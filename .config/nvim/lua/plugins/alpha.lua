2275bc699fd613e05abc1d03bd3f86e573b7878e
Rafael Cavalcanti <dev@rafaelc.org>
2023-08-27 19:17:09 -0300

    vim: Change alpha theme

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -4,7 +4,7 @@
 return  {
   "goolord/alpha-nvim",
   config = function()
-    require("alpha").setup(require("alpha.themes.dashboard").config)
+    require("alpha").setup(require("alpha.themes.startify").config)
   end,
 }
 
