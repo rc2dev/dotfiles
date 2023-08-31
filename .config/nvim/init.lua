35805fdb7632762b9678f20b8165ec8a78bb7b39
Rafael Cavalcanti <dev@rafaelc.org>
2023-08-31 18:02:36 -0300

    Refactor lazy.nvim loading

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,4 @@
 -- init.lua
 -- Author: Rafael Cavalcanti <https://rafaelc.org/dev>
 
--- Must be done before sourcing lazy.nvim
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
-
-if vim.fn.has("nvim-0.8") == 1 then
-  require("lazy-loader")
-end
+require("core.plugins")
---
+++ .config/nvim/lua/core/plugins.lua
@@ -0,0 +1,28 @@
+-- Plugins loading
+-- Author: Rafael Cavalcanti <https://rafaelc.org/dev>
+
+local function load_lazy ()
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  local opts = {}
+  require("lazy").setup("plugins", opts)
+end
+
+-- Must be done before sourcing lazy.nvim
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+if vim.fn.has("nvim-0.8") == 1 then
+  load_lazy()
+end
