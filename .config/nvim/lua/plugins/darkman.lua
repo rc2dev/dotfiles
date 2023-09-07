e7a440f23b093cf739db1f886ae2619f1e29a34b
Rafael Cavalcanti <dev@rafaelc.org>
2023-09-07 15:53:14 -0300

    Minor fix

--- .config/nvim/lua/plugins/darkman.lua
+++ .config/nvim/lua/plugins/darkman.lua
@@ -3,7 +3,7 @@
 
 return {
   '4e554c4c/darkman.nvim',
-  run = 'go build -o bin/darkman.nvim',
+  build = 'go build -o bin/darkman.nvim',
   config = function()
     require 'darkman'.setup()
   end,
