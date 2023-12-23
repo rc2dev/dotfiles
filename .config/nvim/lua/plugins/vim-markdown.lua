616b2ecce28438789a11a65bb355115836f4e357
Rafael Cavalcanti <dev@rafaelc.org>
2023-12-23 18:14:32 -0300

    vim: Workaround gx issue on vim-markdown

--- .config/nvim/lua/plugins/vim-markdown.lua
+++ .config/nvim/lua/plugins/vim-markdown.lua
@@ -13,5 +13,15 @@ return {
 
     -- Try to fix automatic toggling of folding while typing on insert mode
     vim.g.vim_markdown_folding_style_pythonic = 1
+
+    -- Don't set default mappings, as gx stopped working.
+    -- We'll define them manually below, excluding gx and ge:
+    vim.g.vim_markdown_no_default_key_mappings = 1
+    vim.api.nvim_set_keymap("n", "]]", "<Plug>Markdown_MoveToNextHeader", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("n", "[[", "<Plug>Markdown_MoveToPreviousHeader", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("n", "][", "<Plug>Markdown_MoveToNextSiblingHeader", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("n", "[]", "<Plug>Markdown_MoveToPreviousSiblingHeader", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("n", "]u", "<Plug>Markdown_MoveToParentHeader", { noremap=true, silent=true })
+    vim.api.nvim_set_keymap("n", "]h", "<Plug>Markdown_MoveToCurHeader", { noremap=true, silent=true })
   end
 }
