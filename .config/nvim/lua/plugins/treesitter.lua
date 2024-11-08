996be8d4a5ee71fedac8833f067836309e4d3305
Rafael Cavalcanti <dev@rafaelc.org>
2024-11-08 18:29:03 -0300

    vim: Add treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+-- treesitter config
+-- Author: Rafael Cavalcanti <https://rafaelc.org/dev>
+
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "yaml" },
+          sync_install = false,
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
+}
