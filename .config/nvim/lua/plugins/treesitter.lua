8f76a49cbe0acef9ea80783033e51d1bf73f0735
Rafael Cavalcanti <dev@rafaelc.org>
2024-11-23 11:15:56 -0300

    vim: Update treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "yaml" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "yaml", "diff" },
           sync_install = false,
           auto_install = true,
           highlight = { enable = true },
