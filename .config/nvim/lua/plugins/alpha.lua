b0b52d23ee01029daa9c4b3fbe73921d6a88147c
Rafael Cavalcanti <dev@rafaelc.org>
2025-04-20 22:08:00 -0300

    vim: Add alpha plugin

---
+++ .config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,8 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = { 'echasnovski/mini.icons' },
+  config = function ()
+    require'alpha'.setup(require'alpha.themes.startify'.config)
+  end
+}
+
