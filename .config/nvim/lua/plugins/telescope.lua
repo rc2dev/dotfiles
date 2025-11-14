b34dc6fd1866f5fa0cc39c2561bcbf8932440cdd
Rafael Cavalcanti <dev@rafaelc.org>
2025-11-14 14:07:40 -0300

    vim: Update telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,9 +9,9 @@ return {
     { 'nvim-lua/plenary.nvim' },
 
     -- Extension to use fzf as fuzzy finder
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build =  'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    { 
+      'nvim-telescope/telescope-fzf-native.nvim', 
+      build = 'make' 
     }
   },
 
